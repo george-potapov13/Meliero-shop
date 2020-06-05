23344e86488549e4c81980954092cd21cd8d7da9
george-potapov13 <bayjel13@gmail.com>
2020-06-05 18:17:21 +0300

    add redirect to shop/

--- shop/urls.py
+++ shop/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path
 from . import views
+from django.views.generic import RedirectView
 
 app_name = 'shop'
 urlpatterns = [
-    path('', views.home_page, name='home_page'),
     path('shop/', views.product_list, name='product_list'),
+    path('home/', views.home_page, name='home_page'),
     path('<slug:category_slug>/', views.product_list,
          name='product_list_by_category'),
     path('<int:id>/<str:slug>/', views.product_detail, name='product_detail')
+]
+
+urlpatterns += [
+    path('', RedirectView.as_view(url='/shop/', permanent=True)),
 ]
