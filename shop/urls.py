0d390d9c5d70973f1039815281b44d4d31833b2e
george-potapov13 <bayjel13@gmail.com>
2020-06-05 18:23:19 +0300

    add redirect to shop/

--- shop/urls.py
+++ shop/urls.py
@@ -6,9 +6,10 @@ app_name = 'shop'
 urlpatterns = [
     path('shop/', views.product_list, name='product_list'),
     path('home/', views.home_page, name='home_page'),
-    path('<slug:category_slug>/', views.product_list,
+    path('shop/<slug:category_slug>/', views.product_list,
          name='product_list_by_category'),
-    path('<int:id>/<str:slug>/', views.product_detail, name='product_detail')
+    path('shop/product/<int:id>/<str:slug>/',
+         views.product_detail, name='product_detail')
 ]
 
 urlpatterns += [
